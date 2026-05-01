477d05479ca8fb84f8aca25af69b51aca0d33a85
Nooblet2574 <136353964+Nooblet2574@users.noreply.github.com>
2026-04-30 22:56:12 -0400

    adding final files
    4/?

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$windows = "filename"; //zip
+$android = "filename"; //apk
+$tools = "toolspage";
+$demonlist = "demonlist";
+$youtube = "youtubeprofile";
+$discord = "discordserver";
+$about = "Literally an NSFW GDPS";
+?>
